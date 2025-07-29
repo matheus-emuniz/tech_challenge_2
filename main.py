95809e6a898a401e94e5875451fa6f6811d9f1ab
Matheus <matheustheo2010@gmail.com>
2025-07-29 12:08:01 -0300

    implementing dynamic form

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import random
-import string
-import math
+import time
 
 from genetic_algorithm import GeneticAlgorithm
 
 st.title("Genetic algorithm")
 
-
-col1, col2 = st.columns(2)
-
-with col1:
-    with st.form("config_form"):
-        st.header("Password configuration")
+with st.form("config_form"):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Password configuration")
 
         length = st.slider("Max length", 8, 64, value=32)
         upper = st.checkbox("Uppercase", value=True)
@@ -22,7 +17,8 @@ with col1:
         digits = st.checkbox("Digits", value=True)
         special = st.checkbox("Special characters", value=True)
 
-        st.header("Algorithm configuration")
+    with col2:
+        st.subheader("Algorithm configuration")
 
         generations = st.slider("Generations", 1, 2000, value=1000)
         mutation_rate = st.slider(
@@ -31,27 +27,20 @@ with col1:
         tournament_size = st.slider("Tournament size", 1, 10, value=5)
         population_size = st.slider("Population size", 1, 500, value=100)
 
-        submit = st.form_submit_button("Submit")
-
-
-with col2:
-    chart_data = pd.DataFrame(
-        {
-            "col1": np.random.randn(20),
-            "col2": np.random.randn(20),
-            "col3": np.random.choice(["A", "B", "C"], 20),
-        }
-    )
-
-    st.line_chart(chart_data, x="col1", y="col2", color="col3")
-
+    submit = st.form_submit_button("Submit")
 
 if submit:
+    initial_df = pd.DataFrame({ 'generations': [0], 'best_fitness': [0.0] }, columns=['generations', 'best_fitness'])
+    line_chart = st.line_chart(initial_df, x="generations", y="best_fitness")
+
     ga = GeneticAlgorithm(max_length=length, population_size=population_size, generations=generations,
                           mutation_rate=mutation_rate, tournament_size=tournament_size,
                           uppercase=upper, lowercase=lower, digits=digits, special=special)
 
     results = ga.genetic_algorithm()
 
-    for result in results:
-        print(result)
+    for best_fitness, best_password, generations in results:
+        time.sleep(0.2)
+        new_row = pd.DataFrame({ 'generations': [generations], 'best_fitness': [best_fitness] }, columns=['generations', 'best_fitness'])
+        st.session_state.best_password = best_password
+        line_chart.add_rows(new_row)
